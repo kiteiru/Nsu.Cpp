4649e04b7a4d76c920b1fb3073fa61c3bc467cac
kiteiru <70785965+kiteiru@users.noreply.github.com>
2021-01-25 14:11:33 +0700

    Update Calculator.cpp

--- lab2/StackCalculator/Calculator.cpp
+++ lab2/StackCalculator/Calculator.cpp
@@ -2,6 +2,7 @@
 #include "CalculatorOperation.h"
 #include "OperationCreatorImpl.h"
 #include <sstream>
+#include <string>
 
 void Calculator::calculate(std::istream &stream) {
     std::string currentLine;
@@ -17,35 +18,11 @@ void Calculator::calculate(std::istream &stream) {
             arguments.push_back(currentArg);
         }
         try {
-            std::unique_ptr<Calculator>
-        }
-    }
-    for (std::string currentLine; !stream.eof(); std::getline(stream, currentLine)) {
-        std::stringstream lineStream(currentLine);
-        std::string command;
-        lineStream >> command;
-
-        if (command[0] == '#' || command.empty()) {
-            continue;
-        } else if (command == "EXIT") {
-            break;
-        }
-
-        std::list<std::string> args;
-        while (!lineStream.eof()) {
-            std::string argument;
-            lineStream >> argument;
-            if (argument[0] == '#') {
-                break;
-            }
-            args.push_back(argument);
-        }
-        try {
             std::unique_ptr<CalculatorOperation> operation = Factory::getFactory().getOperation(command);
             operation->run(args, context);
         }
         catch (CalledExceptions &exception) {
-            std::cout << "Exception: " << exception.what() << std::endl;
+            std::cout << "WARNING: " << exception.what() << " exception!" << std::endl;
         }
     }
 }
